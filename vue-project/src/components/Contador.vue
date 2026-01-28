01a78d4a96d552eb9fdf486a7f23eb65cc5bd7be
Ismael <ismael.juarez@ferrox.com.mx>
2026-01-28 16:50:31 -0700

    no message

---
+++ vue-project/src/components/Contador.vue
@@ -0,0 +1,27 @@
+<template>
+<h1>Hola maistro!</h1>
+<p>{{ contador }}</p>
+<button @click="incrementar">Incrementa</button>
+<button @click="decrementar">Decrementa</button>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+const contador = ref(0);
+const incrementar = () =>{
+  contador.value++;
+}
+const decrementar = () =>{
+  contador.value--;
+}
+</script>
+
+<style scoped>
+  h1{
+    font-size: 75px;
+  }
+  p{
+    color: red;
+    font-size: 65px;
+  }
+</style>
---
+++ vue-project/src/components/ListaDeTareas.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h2>Lista de tareas</h2>
+        <input type="text" v-model="nuevaTarea" @keyup.enter="addTarea">
+        <ul>
+            <li v-for="(tarea, index) in tarea" :key="index">
+                {{ tarea }}
+                <button @click="borrarTarea">Borrar</button>
+            </li>
+        </ul>
+    </div>
+</template>
+<script setup>
+import {ref} from 'vue';
+const tarea = ref([]);
+const nuevaTarea = ref('');
+const addTarea = () =>{
+    tarea.value.push(nuevaTarea.value);
+    nuevaTarea.value='';
+}
+const borrarTarea = (index)=>{
+    tarea.value.splice(index,1);
+}
+
+</script>
+<style></style>
